08725ae743ef63db3684ddbcdbf3743c775a1c39
mfarzamalam <mfarzamalam@gmail.com>
2021-04-30 01:47:13 +0500

    forgot password done

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from django.utils.translation import gettext, gettext_lazy as _
 from .models import User
 from django.contrib.auth import password_validation
@@ -31,6 +31,26 @@ class UserPasswordChange(PasswordChangeForm):
         strip=False,
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'autofocus': True, 'class':'form-control'}),
     )
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class':'form-control'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class':'form-control'}),
+    )
+
+class UserPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email', 'class':'form-control'})
+    )
+
+class UserSetPasswordForm(SetPasswordForm):
     new_password1 = forms.CharField(
         label=_("New password"),
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class':'form-control'}),
--- app/urls.py
+++ app/urls.py
@@ -3,18 +3,27 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import UserLoginForm, UserPasswordChange
+from .forms import UserLoginForm, UserPasswordChange, UserPasswordResetForm, UserSetPasswordForm
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('product-detail/<int:pk>', views.product_detail.as_view(), name='product-detail'),
+    
     path('mobile/', views.mobile.as_view(), name='mobile'),
     path('mobile/<slug:brand>', views.mobile.as_view(), name='mobile-brand'),
+    
     path('registration/', views.customerregistration.as_view(), name='customerregistration'),
+    
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=UserLoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    
     path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=UserPasswordChange, success_url = '/changepasswordDone/'), name='changepassword'),
     path('changepasswordDone/', auth_views.PasswordChangeView.as_view(template_name='app/changepasswordDone.html'), name='changepasswordDone'),
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=UserPasswordResetForm), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=UserSetPasswordForm), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
     
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
